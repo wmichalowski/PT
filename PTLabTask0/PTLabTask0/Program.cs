febfa9c1b5ce3a791a8461e368ad3742a384d922
wmichalowski <242222@edu.p.lodz.pl>
2023-03-20 18:03:52 +0100

    initial files and part of task 0

---
+++ PTLabTask0/PTLabTask0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using PTLabTask0;
+class Hello
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        double addition = calc.Add(2, 2);
+        Console.WriteLine("2+2="+ addition);
+    }
+}
