905c4495d35dd5195f510ed30007331850e3cf01
wmichalowski <242222@edu.p.lodz.pl>
2023-03-30 17:40:04 +0200

    prepared basic file structure for task 1

---
+++ PTLabTask0/PTLabTask0/DataLayer/Events.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    internal class BookCheckout
+    {
+
+    }
+
+    internal class BookReturn
+    {
+
+    }
+
+    internal class BookAcquisition
+    {
+
+    }
+
+    internal class BookDeletion
+    {
+
+    }
+}
---
+++ PTLabTask0/PTLabTask0/DataLayer/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    internal class Readers
+    {
+
+    }
+
+    internal class Suppliers
+    {
+
+    }
+
+    internal class Employees
+    {
+
+    }
+}
