5c7befc9e72b9b09c2275264a6c902179d766521
Aleksandra Mikołajczyk <242223@edu.p.lodz.pl>
2023-04-16 23:56:21 +0200

    Added tests

---
+++ PTLabTask0/PTLabTask0/PTLabTask0Test/LogicEventsTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using LogicLayer.Implementation;
+using DataLayer.API;
+using DataLayer.Implementation;
+
+namespace LogicLayer.Tests
+{
+    [TestClass]
+    public class BookRentTests
+    {
+        [TestMethod]
+        public void BookRent_Constructor_InitializesProperties()
+        {
+            // Arrange
+            string readerId = "Reader1";
+            string bookId = "100"; 
+            string employeeId = "Employee1";
+            DateTime timestamp = DateTime.Now;
+            IBook newBook = new Book("Dogs", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+
+            Assert.IsTrue(newBook.Available);
+
+            // Act
+            BookRent bookRent = new BookRent(readerId, bookId, employeeId, timestamp);
+
+            // Assert
+            Assert.IsFalse(newBook.Available);
+            Assert.AreEqual(readerId, bookRent.ReaderId);
+            Assert.AreEqual(bookId, bookRent.BookId);
+            Assert.AreEqual(employeeId, bookRent.EmployeeId);
+            Assert.AreEqual(timestamp, bookRent.Timestamp);
+        }
+    }
+
+    [TestClass]
+    public class BookReturnTests
+    {
+        [TestMethod]
+        public void TestBookReturn()
+        {
+            // Arrange
+            string readerId = "reader1";
+            string bookId = "book1";
+            string employeeId = "employee1";
+            DateTime timestamp = new DateTime(2023, 4, 15);
+            BookReturn bookReturn = new BookReturn(readerId, bookId, employeeId, timestamp);
+
+            // Act
+            bookReturn.ReaderId = "reader2";
+            bookReturn.BookId = "book2";
+            bookReturn.EmployeeId = "employee2";
+            bookReturn.Timestamp = new DateTime(2023, 4, 16);
+
+            // Assert
+            Assert.AreEqual("reader2", bookReturn.ReaderId);
+            Assert.AreEqual("book2", bookReturn.BookId);
+            Assert.AreEqual("employee2", bookReturn.EmployeeId);
+            Assert.AreEqual(new DateTime(2023, 4, 16), bookReturn.Timestamp);
+        }
+    }
+
+    [TestClass]
+    public class BookAcquisitionTests
+    {
+        [TestMethod]
+        public void TestBookAcquisition()
+        {
+            // Arrange
+            string supplierId = "Supplier1";
+            string bookId = "100";
+            IBook newBook = new Book("Dogs", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+            string employeeId = "Employee1";
+            DateTime timestamp = new DateTime(2023, 4, 15);
+
+            // Act
+            BookAcquisition bookAcquisition = new BookAcquisition(supplierId, bookId, newBook, employeeId, timestamp);
+
+            // Assert
+            Assert.AreEqual("Supplier2", bookAcquisition.SupplierId);
+            Assert.AreEqual("book2", bookAcquisition.BookId);
+            Assert.AreEqual("Employee2", bookAcquisition.EmployeeId);
+            Assert.AreEqual(new DateTime(2023, 4, 16), bookAcquisition.Timestamp);
+            Assert.AreEqual("book2", bookAcquisition.Book.Title);
+        }
+    }
+}
+
+        
+    
---
+++ PTLabTask0/PTLabTask0/PTLabTask0Test/LogicTest.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using DataLayer.API;
+using DataLayer.Implementation;
+using LogicLayer.API;
+
+namespace LogicLayer.Implementation.Tests
+{
+    [TestClass]
+    public class LogicTests
+    {
+        private IDataRepository _dataRepository;
+        private ILogic _logic;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _logic = new Logic(_dataRepository);
+        }
+
+        [TestMethod]
+        public void AddBook_BookDoesNotExist_AddsBook()
+        {
+            // Arrange
+            IBook book = new Book("Dogs", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+
+            // Act
+            _logic.AddBook(book);
+
+            // Assert
+            var result = _dataRepository.GetBookById(book.BookId);
+            Assert.AreEqual(book, result);
+            Assert.AreEqual(book, result);
+            Assert.AreEqual(book.Title, result.Title);
+            Assert.AreEqual(book.Author, result.Author);
+            Assert.AreEqual(book.Publisher, result.Publisher);
+            Assert.AreEqual(book.Category, result.Category);
+            Assert.AreEqual(book.Available, result.Available);
+        }
+
+        [TestMethod]
+        public void AddBook_BookExists_DoesNotAddBook()
+        {
+            // Arrange
+            IBook book = new Book("Dogs", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+
+            _dataRepository.AddBook(book);
+
+            // Act
+            _logic.AddBook(book);
+
+            // Assert
+            var result = _dataRepository.GetBookById(book.BookId);
+            Assert.AreEqual(book, result);
+        }
+
+        [TestMethod]
+        public void UpdateBook_BookExists_UpdatesBook()
+        {
+            // Arrange
+            IBook book = new Book("Dogs", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+
+            _dataRepository.AddBook(book);
+
+            IBook bookUpdated = new Book("Cats", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+
+            // Act
+            _logic.UpdateBook(bookUpdated);
+
+            // Assert
+            var result = _dataRepository.GetBookById(book.BookId);
+            Assert.AreEqual(bookUpdated, result);
+            Assert.AreEqual(bookUpdated.Title, result.Title);
+            Assert.AreEqual(bookUpdated.Author, result.Author);
+            Assert.AreEqual(bookUpdated.Publisher, result.Publisher);
+            Assert.AreEqual(bookUpdated.Category, result.Category);
+            Assert.AreEqual(bookUpdated.Available, result.Available);
+        }
+
+        [TestMethod]
+        public void UpdateBook_BookDoesNotExist_DoesNotUpdateBook()
+        {
+            // Arrange
+            IBook book = new Book("Dogs", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+
+            IBook bookUpdated = new Book("Cats", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+
+            // Act
+            _logic.UpdateBook(bookUpdated);
+
+            // Assert
+            var result = _dataRepository.GetBookById(book.BookId);
+            Assert.AreEqual(book, result);
+        }
+
+        [TestMethod]
+        public void DeleteBook_BookExists_DeletesBook()
+        {
+            // Arrange
+            IBook book = new Book("Dogs", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+
+            _dataRepository.AddBook(book);
+
+            // Act
+            _logic.DeleteBook(book.BookId);
+
+            // Assert
+            var result = _dataRepository.GetBookById(book.BookId);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void DeleteBook_BookDoesNotExist_DoesNotDeleteBook()
+        {
+            // Arrange
+            IBook book = new Book("Dogs", "Alice Smith", "100", "CoolPublisher", "Encyclopedia", true);
+
+            // Act
+            _logic.DeleteBook(book.BookId);
+
+            // Assert
+            var result = _dataRepository.GetBookById(book.BookId);
+            Assert.AreEqual(book, result);
+
+        }
+    }
+}
