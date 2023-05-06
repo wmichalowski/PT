9162fce34d2e8dd995679bcabadc6fcac19fcfac
OlaMikolajczyk <242223@edu.p.lodz.pl>
2023-05-06 20:34:34 +0200

    Added data generation

---
+++ PTLabTask0/PTLabTask0/DataLayer/Implementation/EmptyGenerate.cs
@@ -0,0 +1,17 @@
+using DataLayer.API;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Implementation
+{
+    internal class EmptyGenerate: IDataGenerator
+    {
+        public void Generate(IDataRepository dataRepository)
+        {
+        }
+    }
+}
